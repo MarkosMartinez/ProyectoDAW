5cc55dc5819fe96a401e578f988eddc0474f948a
Chris2609 <megustas260904@gmail.com>
2024-03-23 13:37:02 +0100

    cambiar el tamaño del relleno del canvas manual al tamaño del canvas

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-ctx.fillRect(0, 0, 1024, 576);
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 // Creamos una constante para el juygador1 utilizando la clase Sprite, pasandole el parametro de posicion como un objeto con con las coordenadas x,y en pixeles
 const jugador1 = new Sprite({
     x: 50,
-    y: 576 - 200
+    y: 100
 }, "green");
 
 jugador1.pruebaMostrarPersonaje();
 
 const jugador2 = new Sprite({
-    x: 1024 - 150,
-    y: 576 - 200
+    x: canvas.width - 150,
+    y: 100
 }, "red");
 
 jugador2.pruebaMostrarPersonaje();
